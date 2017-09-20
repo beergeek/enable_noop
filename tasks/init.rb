96d33c7a6f57c7bba6e9fa037e5e383812996e2c
beergeek <brett@puppet.com>
2017-09-20 15:25:42 +1000

    Provide feedback on status

--- tasks/init.rb
+++ tasks/init.rb
@@ -6,5 +6,7 @@ begin
   cmd_string = "puppet config set noop true --section agent"
   _stdout, stderr, status = Open3.capture3(cmd_string)
   raise Puppet::Error, stderr if status != 0
-  { status: status }
+  cmd_string = "puppet config print noop --section agent"
+  _stdout, stderr, status = Open3.capture3(cmd_string)
+  puts "Noop is now #{_stdout}"
 end
