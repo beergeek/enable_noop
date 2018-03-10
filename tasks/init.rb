f536d79f4d4cf20b257ea05d98070c91a937dd9c
beergeek <brett@puppet.com>
2018-03-11 08:37:45 +1100

    Update code and metadata

--- tasks/init.rb
+++ tasks/init.rb
@@ -3,10 +3,10 @@ require 'open3'
 require 'puppet'
 
 begin
-  cmd_string = "puppet config set noop true --section agent"
+  cmd_string = 'puppet config set noop true --section agent'
   _stdout, stderr, status = Open3.capture3(cmd_string)
   raise Puppet::Error, stderr if status != 0
-  cmd_string = "puppet config print noop --section agent"
-  _stdout, stderr, status = Open3.capture3(cmd_string)
-  puts "Noop is now #{_stdout}"
+  cmd_string = 'puppet config print noop --section agent'
+  stdout, _stderr, _status = Open3.capture3(cmd_string)
+  puts "Noop is now #{stdout}"
 end
